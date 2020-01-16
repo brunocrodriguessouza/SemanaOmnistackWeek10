11df3a6b72a0e823cf14d4f5670c31d09db2f806
Bruno Souza <brunoprogramadorjava@gmail.com>
2020-01-16 19:12:39 -0300

    [aula3] Finalização do Layout do Sidebar

---
+++ web/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+
+const App = () => {
+  return (
+    <div id='app'>
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required></input>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
