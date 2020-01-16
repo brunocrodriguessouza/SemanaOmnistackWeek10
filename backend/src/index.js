b3c8eee35c28553416bd4c55213eb0d26c39c324
Bruno Souza <brunoprogramadorjava@gmail.com>
2020-01-16 10:56:41 -0300

    [aula2] Criando a base da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-vzukq.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de Parâmetros:
+
+// Query Params: request.query (Filtros, ordenação, paginação, ...) /users?search=Bruno exemplo: request.query
+// Route Params: request.params (Identificar um recurso na alteração ou remoção) /users/:id  exemplo: request.params
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+// MongoDB (Não-relacional)
