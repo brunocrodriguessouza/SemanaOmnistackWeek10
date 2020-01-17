ed9c2d82a1e5dc9e712b47c1c52b064213aeb4a1
Bruno Souza <brunoprogramadorjava@gmail.com>
2020-01-17 03:27:30 -0300

    [aula3] Finalização da interface web e refactory mantendo a funcionalidade

--- web/src/App.js
+++ web/src/App.js
@@ -1,86 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
 const App = () => {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id='app'>
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required></input>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required></input>
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required></input>
-            </div>
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required></input>
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/1760665?s=460&v=4" alt="Bruno Rodrigues" />
-              <div className="user-info">
-                <strong>Bruno Rodrigues</strong>
-                <span>ReactJS, React Native, NodeJS</span>
-              </div>
-              <p>Fullstack in Venturus, In Love with Java and Javascript</p>
-              <a href="https://github/com/brunocrodriguessouza">Acessar Perfil no Github</a>
-            </header>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/1760665?s=460&v=4" alt="Bruno Rodrigues" />
-              <div className="user-info">
-                <strong>Bruno Rodrigues</strong>
-                <span>ReactJS, React Native, NodeJS</span>
-              </div>
-              <p>Fullstack in Venturus, In Love with Java and Javascript</p>
-              <a href="https://github/com/brunocrodriguessouza">Acessar Perfil no Github</a>
-            </header>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/1760665?s=460&v=4" alt="Bruno Rodrigues" />
-              <div className="user-info">
-                <strong>Bruno Rodrigues</strong>
-                <span>ReactJS, React Native, NodeJS</span>
-              </div>
-              <p>Fullstack in Venturus, In Love with Java and Javascript</p>
-              <a href="https://github/com/brunocrodriguessouza">Acessar Perfil no Github</a>
-            </header>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/1760665?s=460&v=4" alt="Bruno Rodrigues" />
-              <div className="user-info">
-                <strong>Bruno Rodrigues</strong>
-                <span>ReactJS, React Native, NodeJS</span>
-              </div>
-              <p>Fullstack in Venturus, In Love with Java and Javascript</p>
-              <a href="https://github/com/brunocrodriguessouza">Acessar Perfil no Github</a>
-            </header>
-          </li>
+          {
+            devs.map(dev => (
+              <DevItem key={dev._id} dev={dev} />
+            ))
+          }
         </ul>
       </main>
     </div>
